3b15f93e8f970eaf22d59ca61d7bcd5565cb832b
HennessyZazzz <60851577+HennessyZazzz@users.noreply.github.com>
2021-08-18 15:46:54 +0300

    Add project files.

---
+++ WebAPI/Data/AppDbInitializer.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebAPI.Data
+{
+    public class AppDbInitializer
+    {
+        public static void Seed(IApplicationBuilder applicationBuilder)
+        {
+            using (var serviceScope = applicationBuilder.ApplicationServices.CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetService<ApplicationDbContext>();
+
+                if(!context.Book.Any())
+                {
+                    context.Book.AddRange(
+                        new Models.Book()
+                        {
+                            Title = "Beginning C++20",
+                            Description = "From Novice to Professional",
+                            Author = "Horton, Ivor, Van Weert, Peter",
+                            IsRead = true,
+                            Rate = 4,
+                            DateRead = DateTime.Now.AddDays(-24),
+                            Genre = "Programing",
+                            ImageUrl = "https://media.springernature.com/w153/springer-static/cover/book/9781484258842.jpg",
+                            DataAdded = DateTime.Now.AddDays(-48)
+                        },
+
+                        new Models.Book()
+                        {
+                            Title = "Beginning T-SQL",
+                            Description = "A Step-by-Step Approach",
+                            Author = "Kellenberger, Kathi, Everest, Lee",
+                            IsRead = false,
+                            Genre = "Programing",
+                            ImageUrl = "https://media.springernature.com/w153/springer-static/cover/book/9781484266069.jpg",
+                            DataAdded = DateTime.Now.AddDays(-48)
+                        });
+                    context.SaveChanges();
+                }
+            }
+        }
+    }
+}
---
+++ WebAPI/Migrations/20210818122740_InitBook3.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebAPI.Migrations
+{
+    public partial class InitBook3 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Genre",
+                table: "Book",
+                type: "nvarchar(max)",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Genre",
+                table: "Book");
+        }
+    }
+}
